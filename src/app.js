111935d0152deab3527b307ad3988e8af375afd8
Moumita Das <moumita@Moumitas-MacBook-Pro.local>
2021-06-01 06:17:56 +0530

    Indent Code. Modify folder structure. Add gitignore . Keep data private

---
+++ src/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const expressFormidable = require("express-formidable");
+const path = require("path");
+const fs = require("fs");
+
+//checking required files
+if (!fs.existsSync("./private/images")) {
+  fs.mkdirSync("./private/images");
+}
+if (!fs.existsSync("./private/data/galleryDetails.json")) {
+  fs.writeFileSync("./private/data/galleryDetails.json", "[]");
+}
+
+//get the previous gallery data
+var galleryDetails = require("../private/data/galleryDetails.json");
+
+//init express
+const app = express();
+
+//add middleware
+app.use(
+  expressFormidable({
+    uploadDir: path.join(__dirname, "../private/images"),
+    multiples: false,
+  })
+);
+
+//app route
+app.get("/", (req, res) => {
+  console.log(`\nGET request for Home Page at ${new Date().toUTCString()}`);
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.post("/uploadImageAndData", (req, res) => {
+  if (
+    req.fields["title"] == "" ||
+    req.fields["description"] == "" ||
+    req.fields["submittedBy"] == "" ||
+    req.files.img.name == ""
+  ) {
+    console.log(
+      `\nA bad POST request for Image Submission at ${new Date().toUTCString()}`
+    );
+    res.send(
+      "<h1>Your Application has not been Submitted<br>Fill All The Fields Carefully</h1>"
+    );
+  } else {
+    console.log(
+      `\nPOST request no. ${
+        galleryDetails.length + 1
+      } for Image Submission at ${new Date().toUTCString()}`
+    );
+    fs.renameSync(
+      req.files.img.path,
+      path.join(
+        __dirname,
+        "../private/images",
+        "img" +
+          String(galleryDetails.length) +
+          "." +
+          path.parse(req.files.img.name).ext
+      )
+    );
+    galleryDetails.push({
+      imgID: galleryDetails.length,
+      title: req.fields["title"],
+      description: req.fields["description"],
+      submittedBy: req.fields["submittedBy"],
+    });
+    const jsonstr = JSON.stringify(galleryDetails);
+    fs.writeFileSync("./private/data/galleryDetails.json", jsonstr);
+    res.send("<h1>Your Application has been Submitted Successfully</h1>");
+  }
+});
+
+module.exports = app;
